5705adc1b933b69425d52704c0a2d4fed6812ee0
Raviolz <gi.ravioli@gmail.com>
2026-02-06 13:23:46 +0100

    FetchRendered

--- src/Components/MoviesRow.jsx
+++ src/Components/MoviesRow.jsx
@@ -5,6 +5,9 @@ import Col from "react-bootstrap/Col"
 const movieUrl = "https://www.omdbapi.com/?apikey=ac879cbd"
 
 class MoviesRow extends Component {
+  state = {
+    movies: [],
+  }
   componentDidMount() {
     this.getMovies()
   }
@@ -14,6 +17,7 @@ class MoviesRow extends Component {
       .then((response) => response.json())
       .then((data) => {
         console.log("Film recuperati:", data)
+        this.setState({ movies: data.Search })
       })
       .catch((err) => {
         console.log("Errore:", err)
@@ -21,16 +25,18 @@ class MoviesRow extends Component {
   }
 
   render() {
-    ;<>
-      return <h4>Trending Now</h4>;
-      {/*  <Row xs={1} sm={2} md={4} lg={6} className="mb-4">
-          {movies.map((movie, index) => (
-            <Col key={index} className="mb-2 text-center px-1">
-              <img className="img-fluid movie-poster" src={movie} alt="movie picture" />
+    return (
+      <>
+        <h4>Trending Now</h4>
+        <Row xs={1} sm={2} md={4} lg={6} className="mb-4">
+          {this.state.movies.map((movie) => (
+            <Col key={movie.imdbID} className="mb-2 text-center px-1">
+              <img className="img-fluid movie-poster" src={movie.Poster} alt="movie picture" />
             </Col>
           ))}
-        </Row> */}
-    </>
+        </Row>
+      </>
+    )
   }
 }
 export default MoviesRow
