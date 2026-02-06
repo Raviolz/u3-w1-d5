c8c4d4b5181696cac0e125b31cda7f0cd2f4bd6b
Raviolz <gi.ravioli@gmail.com>
2026-02-06 12:44:19 +0100

    MovieRowTestMapStatico

--- src/Components/MoviesRow.jsx
+++ src/Components/MoviesRow.jsx
@@ -4,28 +4,38 @@ import Col from "react-bootstrap/Col"
 
 class MoviesRow extends Component {
   render() {
+    const movies = [
+      //per partire da public uso url piu' veloce
+      "/1.png",
+      "/2.png",
+      "/3.png",
+      "/4.png",
+      "/5.png",
+      "/6.png",
+      "/7.png",
+      "/8.png",
+      "/9.png",
+      "/10.png",
+      "/11.png",
+      "/12.png",
+      "/13.png",
+      "/14.png",
+      "/15.png",
+      "/16.png",
+      "/17.png",
+      "/18.png",
+    ]
+
     return (
       <>
         <h4>Trending Now</h4>
+
         <Row xs={1} sm={2} md={4} lg={6} className="mb-4">
-          <Col className=" mb-2 text-center px-1">
-            <img className="img-fluid" src="assets/1.png" alt="movie picture" />
-          </Col>
-          <Col className=" mb-2 text-center px-1">
-            <img className="img-fluid" src="assets/2.png" alt="movie picture" />
-          </Col>
-          <Col className=" mb-2 text-center px-1">
-            <img className="img-fluid" src="assets/3.png" alt="movie picture" />
-          </Col>
-          <Col className=" mb-2 text-center px-1">
-            <img className="img-fluid" src="assets/4.png" alt="movie picture" />
-          </Col>
-          <Col className=" mb-2 text-center px-1">
-            <img className="img-fluid" src="assets/5.png" alt="movie picture" />
-          </Col>
-          <Col className=" mb-2 text-center px-1">
-            <img className="img-fluid" src="assets/6.png" alt="movie picture" />
-          </Col>
+          {movies.map((movie, index) => (
+            <Col key={index} className="mb-2 text-center px-1">
+              <img className="img-fluid movie-poster" src={movie} alt="movie picture" />
+            </Col>
+          ))}
         </Row>
       </>
     )
